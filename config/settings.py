e4765f1f37173656e88ab0bdfd18651396105799
Maksim Pinchuk <pinchukmaksim013@gmail.com>
2026-07-24 17:18:32 +0200

    feat: connect rest_framework.authtoken to the settings

--- config/settings.py
+++ config/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "bookings",
     "rest_framework",
+    "rest_framework.authtoken",
 ]
 
 MIDDLEWARE = [
@@ -139,4 +140,7 @@ STATIC_URL = "static/"
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
+    ],
 }
